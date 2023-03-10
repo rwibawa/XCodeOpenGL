2e12b2d79a79c6f309d9fedb71c3e33b1184e1a9
Ryan Wibawa <ryan.wibawa@gmail.com>
2023-03-10 12:13:07 -0800

    Initial Commit

---
+++ XCodeOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XCodeOpenGL
+//
+//  Created by Ryan Wibawa on 3/10/23.
+//  Copyright © 2023 navisow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
